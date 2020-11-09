499c3cb0ede59d46692d26acbdf123209660aabd
Andre <andrelujlon@gmail.com>
2020-11-08 23:35:15 -0500

    Audio Reproduction (is not working yet)

--- punto-blanco/ViewControllerAudio.swift
+++ punto-blanco/ViewControllerAudio.swift
@@ -5,37 +5,55 @@
 //  Created by user181032 on 11/4/20.
 //
 
+import AVFoundation
 import UIKit
-
-
 
 class ViewControllerAudio: UIViewController {
 
-    /*
-    var estadoPista = "Pause"
-    @IBOutlet weak var btPlayPause: UIButton!
-    let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
-    */
+    var player: AVAudioPlayer?
+    @IBOutlet weak var playButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+
     
-    /*
-    @objc func fireTimer() {
-        print("Se acabo el tiempo!")
-    }
-    
-    @IBAction func playAudio(_ sender: UIButton) {
-        if estadoPista == "Pause"{
-            btPlayPause.setImage(UIImage(named: "pause-btn"), for: UIControl.State.normal)
-            estadoPista = "Play"
-        }else if estadoPista == "Play"{
-            btPlayPause.setImage(UIImage(named: "play-btn"), for: UIControl.State.normal)
-            estadoPista = "Pause"
+    @IBAction func playButton(_ sender: UIButton) {
+        if let player = player, player.isPlaying{
+            playButton.setImage(UIImage(named: "pause"), for: .normal)
+            player.stop()
+        }
+        else{
+            
+            let urlString = Bundle.main.path(forResource: "meditacion guiada 5 minutos", ofType: "mp3")
+            
+            do {
+                try AVAudioSession.sharedInstance().setMode(.default)
+                try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+                guard let urlString = urlString else {
+                    return
+                }
+                
+                player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: urlString))
+                
+                guard let player = player else{
+                    return
+                }
+                
+                player.play()
+                
+            } catch {
+                print("Algo no esta bien")
+            }
         }
     }
-    */
+    
+    
+    @IBAction func heTerminadoButton(_ sender: UIButton) {
+        
+    }
+    
+    
 }
