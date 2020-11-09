3837f12d213bf5622aaa755b3a6d2e7937cb3b5c
Andre <andrelujlon@gmail.com>
2020-11-08 23:58:31 -0500

    Organizing and starting custom table views cells

---
+++ punto-blanco/TableViewCellMeditaciones.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCellMeditaciones.swift
+//  punto-blanco
+//
+//  Created by user180479 on 11/8/20.
+//
+
+import UIKit
+
+class TableViewCellMeditaciones: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
