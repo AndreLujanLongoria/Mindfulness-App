326986108f5ec42f1f3316534a01c0289b8fb8e9
Andre <andrelujlon@gmail.com>
2020-11-11 17:45:02 -0500

    Adding UserDefaults to "Usuario" view

--- punto-blanco/Scripts views/ViewControllerPerfil.swift
+++ punto-blanco/Scripts views/ViewControllerPerfil.swift
@@ -13,16 +13,24 @@ class ViewControllerPerfil: UIViewController {
     @IBOutlet weak var tfNombre: UITextField!
     @IBOutlet weak var tfCorreo: UITextField!
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Perfil"
         logoutButton.layer.cornerRadius = 20
+        
+        tfNombre.text = defaults.string(forKey: "username")
+        tfCorreo.text = defaults.string(forKey: "correoUsuario")
     }
     
     @IBAction func quitaTeclado () {
         view.endEditing(true)
+        defaults.set(tfNombre.text, forKey: "username")
+        defaults.set(tfCorreo.text, forKey: "correoUsuario")
     }
+    
     
     @IBAction func logoutFromSession(_ sender: UIButton) {
         // ...
