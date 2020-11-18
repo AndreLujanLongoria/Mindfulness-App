2c2954539ada860518414af6762a1e339635ad3e
Rubén López Charles <33010291+RubenLCharles@users.noreply.github.com>
2020-11-18 18:52:30 -0500

    Estadisticas (sin constraints)

--- punto-blanco/Scripts views/ViewControllerEstadisticas.swift
+++ punto-blanco/Scripts views/ViewControllerEstadisticas.swift
@@ -9,6 +9,21 @@ import UIKit
 
 class ViewControllerEstadisticas: UIViewController {
 
+    @IBOutlet weak var progUno: UIProgressView!
+    @IBOutlet weak var progDos: UIProgressView!
+    @IBOutlet weak var progTres: UIProgressView!
+    @IBOutlet weak var progCuatro: UIProgressView!
+    @IBOutlet weak var progCinco: UIProgressView!
+    @IBOutlet weak var progSeis: UIProgressView!
+    @IBOutlet weak var ProgSiete: UIProgressView!
+    let compUno = Progress(totalUnitCount: 200)
+    let compDos = Progress(totalUnitCount: 200)
+    let compTres = Progress(totalUnitCount: 200)
+    let compCuatro = Progress(totalUnitCount: 200)
+    let compCinco = Progress(totalUnitCount: 200)
+    let compSeis = Progress(totalUnitCount: 200)
+    let compSiete = Progress(totalUnitCount: 200)
+    
     var registro = [Usuario]()
     
     // Persistencia Codable
@@ -37,6 +52,7 @@ class ViewControllerEstadisticas: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Estadisticas"
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -47,11 +63,58 @@ class ViewControllerEstadisticas: UIViewController {
         let cantidad = registro.count
         var sum = 0
         
-        for index in 0...cantidad-1 {
-            sum += registro[index].pulsoDesp
+        if cantidad > 0 {
+            for index in 0...cantidad-1 {
+                sum += registro[index].pulsoDesp
+            }
+            let average = sum/cantidad
+            
+            bpmValue.text = String(average)
+        } else {
+            bpmValue.text = String("0")
         }
-        let average = sum/cantidad
         
-        bpmValue.text = String(average)
+        compUno.completedUnitCount = 0
+        compDos.completedUnitCount = 0
+        compTres.completedUnitCount = 0
+        compCuatro.completedUnitCount = 0
+        compCinco.completedUnitCount = 0
+        compSeis.completedUnitCount = 0
+        compSiete.completedUnitCount = 0
+        
+        for index in 0...cantidad-1 {
+            if index == 0 {
+                compUno.completedUnitCount = Int64(registro[index].pulsoDesp)
+            } else if index == 1 {
+                compDos.completedUnitCount = Int64(registro[index].pulsoDesp)
+            } else if index == 2 {
+                compTres.completedUnitCount = Int64(registro[index].pulsoDesp)
+            } else if index == 3 {
+                compCuatro.completedUnitCount = Int64(registro[index].pulsoDesp)
+            } else if index == 4 {
+                compCinco.completedUnitCount = Int64(registro[index].pulsoDesp)
+            } else if index == 5 {
+                compSeis.completedUnitCount = Int64(registro[index].pulsoDesp)
+            } else if index == 7 {
+                compSiete.completedUnitCount = Int64(registro[index].pulsoDesp)
+            }
+        }
+        
+        var aux = Float(compUno.fractionCompleted)
+        progUno.setProgress(aux, animated: true)
+        aux = Float(compDos.fractionCompleted)
+        progDos.setProgress(aux, animated: true)
+        aux = Float(compTres.fractionCompleted)
+        progTres.setProgress(aux, animated: true)
+        aux = Float(compCuatro.fractionCompleted)
+        progCuatro.setProgress(aux, animated: true)
+        aux = Float(compCinco.fractionCompleted)
+        progCinco.setProgress(aux, animated: true)
+        aux = Float(compSeis.fractionCompleted)
+        progSeis.setProgress(aux, animated: true)
+        aux = Float(compSiete.fractionCompleted)
+        ProgSiete.setProgress(aux, animated: true)
+        
+
     }
 }
