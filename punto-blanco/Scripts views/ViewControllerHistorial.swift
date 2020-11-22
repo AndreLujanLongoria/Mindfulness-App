4133f16ebb05f4c52f8c70f70c0b99b3688c69a0
Rubén López Charles <33010291+RubenLCharles@users.noreply.github.com>
2020-11-21 19:24:09 -0500

    Registro en Historial

--- punto-blanco/Scripts views/ViewControllerHistorial.swift
+++ punto-blanco/Scripts views/ViewControllerHistorial.swift
@@ -7,13 +7,61 @@
 
 import UIKit
 
-class ViewControllerHistorial: UIViewController {
+class ViewControllerHistorial: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tablaMedit: UITableView!
+    var registro = [Usuario]()
+    var cantidad : Int!
+    
+    // Persistencia Codable
+    func dataFileURL () -> URL {
+        let url = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+        let pathArchivo = url.appendingPathComponent("registro.plist")
+        
+        return pathArchivo
+    }
+    
+    func obtenerRegistro() {
+        registro.removeAll()
+        do {
+            let data = try Data.init(contentsOf: dataFileURL())
+            registro = try PropertyListDecoder().decode([Usuario].self, from: data)
+            cantidad = registro.count
+        } catch {
+            print("No hay datos aun")
+            cantidad = 0
+        }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return registro.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "minicelda")!
+        cell.textLabel?.text = "Pulsos: " + String(registro[indexPath.row].pulsoAnt) + " -> " + String(registro[indexPath.row].pulsoDesp)
+        
+        return cell
+    }
+    
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Historial"
         // Do any additional setup after loading the view.
+        tablaMedit.delegate = self
+        tablaMedit.dataSource = self
+        obtenerRegistro()
+        tablaMedit.reloadData()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        obtenerRegistro()
+        tablaMedit.reloadData()
     }
     
 
